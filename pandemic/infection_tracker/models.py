8bab8c20be591ffe9e0425d1d17d7443b2ce7bb0
Stefan van der Zanden <svanderzanden@hfm.nl>
2018-06-24 22:35:50 +0200

    Can add/remove city cards and added games so we can save previous data

--- pandemic/infection_tracker/models.py
+++ pandemic/infection_tracker/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 
 
-class CardSpecial(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField()
+class Game(models.Model):
+    date = models.DateField()
+    finished = models.BooleanField(default=False)
+    won = models.NullBooleanField()
 
 
 class City(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
+    @property
+    def active_cards(self):
+        return self.infection_cards.filter(active=True)
+
+    @property
+    def inactive_cards(self):
+        return self.infection_cards.filter(active=False)
+
 
 class InfectionCard(models.Model):
     city = models.ForeignKey('City', related_name='infection_cards', on_delete=models.CASCADE)
-    special = models.OneToOneField('CardSpecial', null=True, blank=True, on_delete=models.SET_NULL)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.city
@@ -21,3 +30,4 @@ class InfectionCard(models.Model):
 class Round(models.Model):
     round_number = models.IntegerField(default=1)
     cards = models.ManyToManyField('InfectionCard', related_name='epidemic_rounds')
+    game = models.ForeignKey('Game', related_name='rounds', on_delete=models.CASCADE, null=True)
--- pandemic/infection_tracker/urls.py
+++ pandemic/infection_tracker/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 
-from infection_tracker.views import TableView, InfectCity, NewRound, AddCityView
+from infection_tracker.views import InfectCity, NewRound, AddCityView, CityListView, UpdateCityCards, \
+    Overview, GameView
 
 app_name = 'infection_tracker'
 
 urlpatterns = [
-    path('', TableView.as_view(), name='list'),
+    path('', Overview.as_view(), name='overview'),
+
+    path('game/<int:id>', GameView.as_view(), name='game'),
+
+    path('city_list', CityListView.as_view(), name='city_list'),
+    path('add_city', AddCityView.as_view(), name='add_city'),
+    path('update_city', UpdateCityCards.as_view(), name='update_city'),
+
     path('infect_city', InfectCity.as_view(), name='infect_city'),
     path('new_round', NewRound.as_view(), name='new_round'),
-    path('add_city', AddCityView.as_view(), name='add_city'),
 ]

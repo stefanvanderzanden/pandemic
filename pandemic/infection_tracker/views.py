cb90bff20a9c84e751ba95e382b9a4445b8a656d
Stefan van der Zanden <svanderzanden@hfm.nl>
2018-07-07 15:02:56 +0200

    Sorting, updated calculations, removing cards from round

--- pandemic/infection_tracker/views.py
+++ pandemic/infection_tracker/views.py
@@ -28,14 +28,28 @@ class GameView(TemplateView):
         return super(GameView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
+
         total_cards = InfectionCard.objects.filter(active=True).count()
         current_round = Round.objects.all().order_by('-round_number').first()
+        rounds = Round.objects.filter(round_number__lt=current_round.round_number)
 
+        # Count the total number of cards drawn, by adding the max drawn cards per city
+        total_cards_drawn = 0
+        for city in City.objects.all():
+            cards_drawn_per_round_per_city = []
+            for r in rounds:
+                cards_drawn_per_round_per_city.append(r.cards.filter(city=city).count())
+
+            city_max = max(cards_drawn_per_round_per_city)
+            total_cards_drawn += city_max
+
+        remaining_total_cards = total_cards_drawn - current_round.cards.all().count()
         data = []
 
         for city in City.objects.all():
             total_city_cards = InfectionCard.objects.filter(city=city, active=True).count()
             city_data = {'name': city.name, 'rounds': [], 'total': total_city_cards}
+
             for r in Round.objects.all():
                 city_data['rounds'].append(r.cards.filter(city=city).count())
 
@@ -45,19 +59,26 @@ class GameView(TemplateView):
                             total_cards - current_round.cards.all().count())) * 100
                 city_data['chance'] = chance
             else:
-                previous_round = Round.objects.get(round_number=current_round.round_number - 1)
-                previous_drawn_cards_count = previous_round.cards.all().count()
-                if current_round.cards.all().count() < previous_drawn_cards_count:
-                    previous_round_city_drawn_count = previous_round.cards.filter(city=city).count()
-                    if previous_round_city_drawn_count > 0:
-                        remaining_city_cards = previous_round_city_drawn_count - current_round.cards.filter(
+                cards_drawn_per_round_per_city = []
+                for r in rounds:
+                    # Calculate max over rounds
+                    cards_drawn_per_round_per_city.append(r.cards.filter(city=city).count())
+                total_cards_drawn_per_city = max(cards_drawn_per_round_per_city)
+                # print('DRAWN FOR %s = %s' % (city.name, total_cards_drawn_per_city))
+
+                if current_round.cards.all().count() < total_cards_drawn:
+                    # We are in the situation we have turned the disposal cards back on top
+                    if total_cards_drawn_per_city > 0:
+                        remaining_city_cards = total_cards_drawn_per_city - current_round.cards.filter(
                             city=city).count()
-                        remaining_total_cards = previous_drawn_cards_count - current_round.cards.all().count()
+                        print('remaining_city_cards: for %s = %s' % (city.name, remaining_city_cards))
+                        print('remaining_total_cards: %s' % remaining_total_cards)
                         chance = (remaining_city_cards / remaining_total_cards) * 100
                         city_data['chance'] = chance
                     else:
                         city_data['chance'] = 0
                 else:
+                    # All disposal cards are drawn, new cards
                     drawn_cards = current_round.cards.filter(city=city).count()
                     chance = ((InfectionCard.objects.filter(city=city).count() - drawn_cards) / (
                             total_cards - current_round.cards.all().count())) * 100
@@ -65,7 +86,12 @@ class GameView(TemplateView):
 
             data.append(city_data)
 
-        sorted_data = sorted(data, key=itemgetter('name'))
+        sorting_key = 'name'
+        if self.request.GET.get('sort'):
+            if self.request.GET.get('sort') in data[0].keys():
+                sorting_key = self.request.GET.get('sort')
+
+        sorted_data = sorted(data, key=itemgetter(sorting_key), reverse=sorting_key == 'chance')
 
         return super(GameView, self).get_context_data(
             data=sorted_data,
@@ -78,17 +104,27 @@ class InfectCity(View):
 
     def post(self, request, *args, **kwargs):
         city_name = request.POST.get('city')
+        action = request.POST.get('action')
+
         current_round = Round.objects.all().order_by('-round_number').first()
         total_amount_for_city = InfectionCard.objects.filter(city__name=city_name).count()
         amount_for_city_in_current_round = current_round.cards.filter(city__name=city_name).count()
 
-        if amount_for_city_in_current_round < total_amount_for_city:
-            card = InfectionCard.objects.exclude(epidemic_rounds__cards__city__name=city_name,
-                                             epidemic_rounds=current_round).filter(city__name=city_name).first()
-            current_round.cards.add(card)
-            return JsonResponse({'message': 'success'})
-        else:
-            return JsonResponse({'message': 'not possible'})
+        message = 'no valid action'
+        if action == 'up':
+            if amount_for_city_in_current_round < total_amount_for_city:
+                card = InfectionCard.objects.exclude(epidemic_rounds__cards__city__name=city_name,
+                                                 epidemic_rounds=current_round).filter(city__name=city_name).first()
+                current_round.cards.add(card)
+                message = 'success'
+
+        elif action == 'down':
+            if amount_for_city_in_current_round >= 1:
+                card = InfectionCard.objects.filter(epidemic_rounds__cards__city__name=city_name,
+                                              epidemic_rounds=current_round).filter(city__name=city_name).first()
+                current_round.cards.remove(card)
+
+        return JsonResponse({'message': message})
 
 
 class NewRound(View):
